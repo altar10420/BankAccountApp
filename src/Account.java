1df96325746aff092195b4f4c4e4cb635c005123
Piotr Śrótwa <altar10420@gmail.com>
2023-05-06 16:37:55 +0200

    adding constructors

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,7 @@
 public abstract class Account implements IRate {
 
+    public Account(String name) {
+        System.out.println("Name: " + name);
+        System.out.print("New account: ");
+    }
 }
--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -2,5 +2,11 @@ public class BankAccountApp {
 
     public static void main(String[] args) {
 
+        Checking chkacc1 = new Checking("Tom Wilson");
+
+        Savings savacc1 = new Savings("Rich Stone");
+
+
+
     }
 }
